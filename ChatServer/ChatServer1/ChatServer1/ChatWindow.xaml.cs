f20f954e8d285b0dce172de1e1fe0e31c44ea739
Dark0mens <jeffhayslett@gmailcom>
2016-04-24 13:10:30 -0500

    test

--- ChatServer/ChatServer1/ChatServer1/ChatWindow.xaml.cs
+++ ChatServer/ChatServer1/ChatServer1/ChatWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
 
 namespace ChatServer1
 {
