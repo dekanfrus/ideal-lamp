457e82d717f95c65bd939540a51f7df224b89c54
dekanfrus <john.alves01@gmail.com>
2016-04-26 12:30:32 -0500

    Add documentation and logging.

--- ChatServer/ChatServer/server.cs
+++ ChatServer/ChatServer/server.cs
@@ -64,17 +64,11 @@ namespace ChatServer
 
         // Salt and Initialization Vector values for encryption of data
         private static byte[] Salt = {2, 123,  61, 217, 205, 133, 176, 171, 164, 248, 215, 129, 232, 210, 145, 56, 
- 45, 133,  55, 137,  95, 174, 245, 179, 205, 140, 190, 215, 110, 122, 169, 95 };
-
+                                      45, 133,  55, 137,  95, 174, 245, 179, 205, 140, 190, 215, 110, 122, 169, 95 };
         private static byte[] IV = {9,  90,  56,  18, 127, 245, 101, 112,  72, 133, 248, 224,  73,  12,  96,  24, };
 
         private static ICryptoTransform Encryptor, Decryptor;
         private static System.Text.UTF8Encoding Encoder;
-
-        public static void SetupEncryption()
-        {
-            
-        }
 
         //***************************************************************************************
         // Function Name: server
@@ -157,10 +151,13 @@ namespace ChatServer
 
             try
             {
+                // Add the client to the client list then create a new state object to store
+                // the data about the client and received messages.
                 clientList.Add(clientHandler);
                 ClientData clientState = new ClientData();
                 clientState.activeListener = clientHandler;
 
+                // Add a log entry for the connected client
                 using (StreamWriter logWriter = File.AppendText("ServerLog.txt"))
                 {
                     logWriter.Write("{0} {1}:  ", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
@@ -171,6 +168,7 @@ namespace ChatServer
                 // Send updated users list to all clients
                 BroadcastUsers();
 
+                // Begin receiving data from the client
                 clientHandler.BeginReceive(clientState.buffer, 0, ClientData.BufferSize, 0, new AsyncCallback(ReceiveData), clientState);
             }
             catch (Exception error)
@@ -192,7 +190,6 @@ namespace ChatServer
         {
 
             // Clear the message string and grab receiving client socket information
-
             ClientData clientState = (ClientData)AsyncResult.AsyncState;
             Socket clientHandler = clientState.activeListener;
             string clientMessage = string.Empty;
@@ -204,16 +201,24 @@ namespace ChatServer
                 // Receive data from client
                 if (dataReceived > 0)
                 {
+                    // Clear any existing data in the client object string
                     clientState.clientString.Clear();
+                    // Append received data to the client object string
                     clientState.clientString.Append(Encoding.ASCII.GetString(clientState.buffer, 0, dataReceived));
 
-                    clientMessage = clientState.clientString.ToString();
+                    // Convert the message from a StringBuilder to string
+                    EncryptedMessage = clientState.clientString.ToString();
 
-                    // Message Decryption Here
+                    // Decrypt the client message
+                    string clientMessage = DecryptData(clientMessage);
 
+                    // Handle Login code
                     if (clientMessage.StartsWith("1000"))
                     {
+                        // Call Login function to query the database and verify credentials
                         int success = Login(clientMessage);
+
+                        // Depending on authentication results, log and proceed
                         switch (success)
                         {
                             case 1:
@@ -235,10 +240,15 @@ namespace ChatServer
                             default:
                                 break;
                         }
-                    }
+                    } // End of Login handler
+
+                    // Handle Register Code
                     else if (clientMessage.StartsWith("2000"))
                     {
+                        // Call Register function and receive result code
                         int success = Register(clientMessage);
+
+                        // Depending on the results from Register, log and proceed
                         switch (success)
                         {
                             case 1:
@@ -276,30 +286,49 @@ namespace ChatServer
                             default:
                                 break;
                         }
-                    }
+                    } // End of Register handler
+
+                    // Placeholder for additional codes
                     else if (clientMessage.StartsWith("3"))
                     {
                         // Do something else
                     }
+
+                    // Broadcast Message Handler
                     else
                     {
+                        // Log the message then call the BroadcastMessage function
                         using (StreamWriter logWriter = File.AppendText("ServerLog.txt"))
                         {
                             logWriter.Write("{0} {1}:  ", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
                             logWriter.Write(clientMessage);
                         }
                         BroadcastMessage(clientMessage);
-                    }
-                }
-            }
+                    }// End of Broadcast Message Hanler
+                }// End of receive data if statement
+            }// End of try block
             catch (Exception error)
             {
                 Console.WriteLine(error.ToString());
             }
 
+            // If the client socket is still connected, call the function recursively
+            // This is necessary otherwise the thread will exit and the client will no longer be able to send data
             if (clientHandler.Connected)
             {
-                clientHandler.BeginReceive(clientState.buffer, 0, ClientData.BufferSize, 0, new AsyncCallback(ReceiveData), clientState);
+                try
+                {
+                    clientHandler.BeginReceive(clientState.buffer, 0, ClientData.BufferSize, 0, new AsyncCallback(ReceiveData), clientState);
+                }
+                catch (Exception error)
+                {
+                    using (StreamWriter logWriter = File.AppendText("ServerLog.txt"))
+                    {
+                        logWriter.Write("{0} {1}:  ", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                        logWriter.Write(error);
+                    }
+                    Console.WriteLine(error.ToString());
+                }
             }
 
             if (!clientHandler.Connected)
@@ -311,20 +340,41 @@ namespace ChatServer
 
         }// End of ReceiveData Function
 
+        //*******************************************************************************************
+        // Function Name: BroadcastMessage                                                         **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
         public static void BroadcastMessage(string recvdMessage)
         {
+            // Output to server console.  Primarily for testing purposes.
+            Console.Write("Plain Text Message: ");
             Console.Write(recvdMessage);
             recvdMessage = encryptData(recvdMessage);
-            Console.Write(recvdMessage);
+            Console.Write("Encrypted Message: ");
+            Console.WriteLine(recvdMessage);
 
+            // Convert the message string to a byte array and then send
+            // all data to all sockets in the client list (list<socket>)
             byte[] data = Encoding.ASCII.GetBytes(recvdMessage);
             foreach (Socket current in clientList)
             {
                 current.BeginSend(data, 0, data.Length, 0, new AsyncCallback(OnBroadcast), current);
             }
-        }
+        }// End of BroadcastMessage function
+
+        //*******************************************************************************************
+        // Function Name: BroadcastUsers                                                           **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
         public static void BroadcastUsers()
         {
+            //Convert List<socket> to a string with ":" delimiter and code 3000
+            //Send string to encryptData() function
+            //Once encrypted, broadcast to all sockets
 
             //recvdMessage = encryptData(recvdMessage);
 
@@ -333,15 +383,30 @@ namespace ChatServer
             //{
             //    current.BeginSend(data, 0, data.Length, 0, new AsyncCallback(OnBroadcast), current);
             //}
-        }
+        }// End of BroadcastUsers function
+
+        //*******************************************************************************************
+        // Function Name: OnBroadcast                                                              **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
         public static void OnBroadcast(IAsyncResult AsyncResult)
         {
             Socket clientSocket = (Socket)AsyncResult.AsyncState;
             clientSocket.EndSend(AsyncResult);
         }
 
+        //*******************************************************************************************
+        // Function Name: Login                                                                    **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
         public static int Login(string loginInfo)
         {
+            // Split the string based on ":" and store in a string array
+            // 
             string[] creds = loginInfo.Split(':');
 
             string userName = creds[1];
@@ -351,8 +416,14 @@ namespace ChatServer
             // was successful, then return true
             // otherwise return error code
             return 1;
-        }
+        }// End of Login function
 
+        //*******************************************************************************************
+        // Function Name: Register                                                                 **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
         public static int Register(string RegisterInfo)
         {
             string[] creds = RegisterInfo.Split(':');
@@ -370,45 +441,74 @@ namespace ChatServer
             return 1;
         }
 
-        public static string encryptData(string message)
+        //*******************************************************************************************
+        // Function Name: EncryptData                                                              **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
+        public static string EncryptData(string message)
         {
+            // Create a new object with the RM (AES) algorithm
             RijndaelManaged AESEncrypt = new RijndaelManaged();
+
+            // Populate the encryptor/decryptor with the salt and initialization vector
             Encryptor = AESEncrypt.CreateEncryptor(Salt, IV);
             Decryptor = AESEncrypt.CreateDecryptor(Salt, IV);
 
+            // Specify the type of encoding we want to use
             Encoder = new System.Text.UTF8Encoding();
 
             try
             {
+                // Convert the message to a byte array
                 Byte[] MessageBytes = Encoding.UTF8.GetBytes(message);
-
+                
+                // Create a memory stream object to stream the message to the crypto stream object
+                // The Crypto stream object requires a stream object - cannot use a byte array directly
                 MemoryStream mStream = new MemoryStream();
                 CryptoStream cStream = new CryptoStream(mStream, Encryptor, CryptoStreamMode.Write);
 
+                // Write the message to the Crypto Stream object encrypting the data
                 cStream.Write(MessageBytes, 0, MessageBytes.Length);
                 cStream.FlushFinalBlock();
 
+                // Read the encrypted data back from the Crypto Stream object
                 mStream.Position = 0;
                 byte[] EncryptedMessage = new byte[mStream.Length];
                 mStream.Read(EncryptedMessage, 0, EncryptedMessage.Length);
 
+                // Close the streams because it's a good idea
                 cStream.Close();
                 mStream.Close();
 
+                // Return the encrypted string back to the calling function
                 return System.Text.Encoding.UTF8.GetString(EncryptedMessage);
             }
             catch (Exception error)
             {
+                // Basic logging.  Send the error to the console and the server log, then return an empty string
                 using (StreamWriter logWriter = File.AppendText("ServerLog.txt"))
                 {
                     logWriter.Write("{0} {1}:  ", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
                     logWriter.Write(error.ToString());
                 }
                 Console.WriteLine(error.ToString());
-                Thread tid = Thread.CurrentThread;
-                return error.ToString();
+                return " ";
             }
         }
+
+        //*******************************************************************************************
+        // Function Name: DecryptData                                                              **
+        // Description:  
+        //               
+        //                                                                                         **
+        //*******************************************************************************************
+        public static string DecryptData(string message)
+        {
+            return System.Text.Encoding.UTF8.GetString(message);
+        }
+
         //*******************************************************************************************
         // Function Name: Main                                                                     **
         // Description:  Main function of the server program, however it just dynamically creates  **
