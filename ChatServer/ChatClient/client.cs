849bb16ae3f3ff5ea86b59d2291dc8895418c44c
Dark0mens <jeffhayslett@gmailcom>
2016-04-26 12:59:42 -0500

    qwerty

--- ChatServer/ChatClient/client.cs
+++ ChatServer/ChatClient/client.cs
@@ -18,7 +18,8 @@ namespace ChatClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new StartUp());
+            Application.Run(ChatClient.StartUp.)
         }
     }
 }
