223f3314c7b61fdd8e2ab94cf084d22300ab7ab7
Dark0mens <jeffhayslett@gmailcom>
2016-05-06 16:39:32 -0500

    Guru Disabled Client Encryption

--- ChatServer/ChatServer1/ChatServer1/Login.xaml.cs
+++ ChatServer/ChatServer1/ChatServer1/Login.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Net.Sockets;
 using System.Net;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace ChatServer1
 {
@@ -34,6 +36,12 @@ namespace ChatServer1
         //int loginFail = 1700;
         Socket s;
         //string serverResponse;
+        private static byte[] Salt = {2, 123,  61, 217, 205, 133, 176, 171, 164, 248, 215, 129, 232, 210, 145, 56,
+                                      45, 133,  55, 137,  95, 174, 245, 179, 205, 140, 190, 215, 110, 122, 169, 95 };
+        private static byte[] IV = { 9, 90, 56, 18, 127, 245, 101, 112, 72, 133, 248, 224, 73, 12, 96, 24, };
+
+        private static ICryptoTransform Encryptor, Decryptor;
+        private static System.Text.UTF8Encoding Encoder;
 
         public Login()
         {
@@ -47,10 +55,61 @@ namespace ChatServer1
             s.Connect(ipAddress, port);
         }
 
+        public static string EncryptData(string message)
+        {
+            // Create a new object with the RM (AES) algorithm
+            RijndaelManaged AESEncrypt = new RijndaelManaged();
+
+            // Populate the encryptor/decryptor with the salt and initialization vector
+            Encryptor = AESEncrypt.CreateEncryptor(Salt, IV);
+            Decryptor = AESEncrypt.CreateDecryptor(Salt, IV);
+
+            // Specify the type of encoding we want to use
+            Encoder = new System.Text.UTF8Encoding();
+
+            try
+            {
+                // Convert the message to a byte array
+                Byte[] MessageBytes = Encoding.UTF8.GetBytes(message);
+
+                // Create a memory stream object to stream the message to the crypto stream object
+                // The Crypto stream object requires a stream object - cannot use a byte array directly
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, Encryptor, CryptoStreamMode.Write);
+
+                // Write the message to the Crypto Stream object encrypting the data
+                cStream.Write(MessageBytes, 0, MessageBytes.Length);
+                cStream.FlushFinalBlock();
+
+                // Read the encrypted data back from the Crypto Stream object
+                mStream.Position = 0;
+                byte[] EncryptedMessage = new byte[mStream.Length];
+                mStream.Read(EncryptedMessage, 0, EncryptedMessage.Length);
+
+                // Close the streams because it's a good idea
+                cStream.Close();
+                mStream.Close();
+
+                // Return the encrypted string back to the calling function
+                return System.Text.Encoding.UTF8.GetString(EncryptedMessage);
+            }
+            catch (Exception error)
+            {
+                // Basic logging.  Send the error to the console and the server log, then return an empty string
+                using (StreamWriter logWriter = File.AppendText("ServerLog.txt"))
+                {
+                    logWriter.Write("{0} {1}:  ", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                    logWriter.Write(error.ToString());
+                }
+                Console.WriteLine(error.ToString());
+                return " ";
+            }
+        }
+
         private void login_send_data()
         {
             //string host = Dns.GetHostName();
-            string loginInformation = "1000:" + userName + ":" + password;
+            string loginInformation = EncryptData("1000:") + userName + EncryptData(":") + password;
             //encrypt loginInformation here
             s.Send(Encoding.UTF8.GetBytes(loginInformation));
         }
@@ -64,6 +123,8 @@ namespace ChatServer1
 
         private void login_submit_Click(object sender, RoutedEventArgs e)
         {
+            userName = EncryptData(userName);
+            password = EncryptData(password);
             //hide the error message label on Log In page for invalid password or user name
             login_error_message.Visibility = Visibility.Collapsed;
 
